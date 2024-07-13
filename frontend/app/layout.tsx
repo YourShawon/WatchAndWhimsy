256dabdfc771a3cd7b4ebf814ae0e1a72a0001b8
YourShawon <yourshawon101@gmail.com>
2024-07-13 21:28:30 +0600

    init

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Watch & Whimsy",
+  description: "A premier e-commerce website specializing in a curated selection of stylish and high-quality watches.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
